7373267f0ae5059dca689cff4300be33e3fdb623
Yura <yura.savaryn.777@gmail.com>
2025-05-20 01:24:23 +0300

    modified server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_ADMIN_CREDENTIALS_PATH);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS_PATH);
 const port = process.env.PORT || 5000;
 
 admin.initializeApp({
